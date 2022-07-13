78cc1fe26f5dee53108ffeeaccdd30d2c9dd0ce6
Idan Maimon <idanmaimon381891@gmail.com>
2022-07-13 19:10:45 +0300

    config fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Toys-app/',
+  base:'/toys-app/',
   plugins: [vue()],
   resolve: {
     alias: {
